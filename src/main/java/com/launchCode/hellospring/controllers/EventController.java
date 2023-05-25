04e1a1950355c7de5579495507c1315b71231364
XGuiG <1351739465@qq.com>
2023-05-25 13:14:05 +0800

    commit exercise13-10

--- src/main/java/com/launchCode/hellospring/controllers/EventController.java
+++ src/main/java/com/launchCode/hellospring/controllers/EventController.java
@@ -8,15 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String> events = new ArrayList<>();
-
     @GetMapping
-    public String list(Model model) {
+    public String displayAllEvents(Model model) {
+        Map<String, String> events = new HashMap<>();
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -26,9 +30,9 @@ public class EventController {
         return "events/create";
     }
 
-    @PostMapping("create")
-    public String CreateEvent(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
-    }
+//    @PostMapping("create")
+//    public String CreateEvent(@RequestParam String eventName) {
+//        events.add(eventName);
+//        return "redirect:";
+//    }
 }
